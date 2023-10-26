be1553ed90d20a1305c6294e3bd4844e1e013a70
kenmaro3 <miha.ken.19@gmail.com>
2023-10-26 10:46:34 +0900

    first commit

---
+++ SwiftWithGoLib/View/Home.swift
@@ -0,0 +1,18 @@
+//
+//  Home.swift
+//  SwiftWithGoLib
+//
+//  Created by Kentaro Mihara on 2023/10/26.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Home()
+}
