f56ccaf8002af20eec104fe9fbecdd7937f9abc8
kenmaro3 <miha.ken.19@gmail.com>
2023-10-26 10:28:00 +0900

    Initial Commit

---
+++ SwiftWithGoLib/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftWithGoLib
+//
+//  Created by Kentaro Mihara on 2023/10/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftWithGoLib/SwiftWithGoLibApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftWithGoLibApp.swift
+//  SwiftWithGoLib
+//
+//  Created by Kentaro Mihara on 2023/10/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftWithGoLibApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
